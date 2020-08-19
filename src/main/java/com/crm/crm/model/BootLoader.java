2b07cbe318ca96a7b88ada4566b60b51eddb61f5
Your Name <j.hamwijk34@gmail.com>
2020-08-19 13:04:51 +0200

    Upload Files

---
+++ src/main/java/com/crm/crm/model/BootLoader.java
@@ -0,0 +1,21 @@
+package com.crm.crm.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootLoader implements CommandLineRunner {
+
+    private final ContactRepository repository;
+
+    @Autowired
+    public BootLoader(ContactRepository repository){
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.repository.save(new Contact("Jordy","Hamwijk","j.hamwijk34@gmail.com"));
+    }
+}

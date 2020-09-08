e6bb3e703dbb5ae063bc5bf895319c614b77c5c1
Your Name <j.hamwijk34@gmail.com>
2020-09-08 22:22:49 +0200

    Update BootLoader.java

--- src/main/java/com/crm/crm/model/BootLoader.java
+++ src/main/java/com/crm/crm/model/BootLoader.java
@@ -3,15 +3,14 @@ package com.crm.crm.model;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
-import org.springframework.stereotype.Controller;
-
-@Controller
+@Component
 @RequiredArgsConstructor //Generates a constructor with required arguments.
 public class BootLoader implements CommandLineRunner {
 
     private final ContactRepository repository;
-    
+
     @Override
     public void run(String... args) throws Exception {
         this.repository.save(new Contact("Jordy","Hamwijk","j.hamwijk34@gmail.com"));

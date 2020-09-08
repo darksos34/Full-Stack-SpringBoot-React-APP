a1a367c63cd1bafac5175b0a64fb78cc2acf1900
Your Name <j.hamwijk34@gmail.com>
2020-09-08 12:25:20 +0200

    Update

--- src/main/java/com/crm/crm/model/ReactAndSpringDataRestApplication.java
+++ src/main/java/com/crm/crm/model/ReactAndSpringDataRestApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReactAndSpringDataRestApp {
-    public static void main (String [] args){
-        SpringApplication.run(ReactAndSpringDataRestApp.class, args);
+public class ReactAndSpringDataRestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
     }
 }

17e4c120f41bed1264920d0a0d28c588fc1f672b
Your Name <j.hamwijk34@gmail.com>
2020-09-09 10:01:33 +0200

    Update Contacts.js

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,50 +1,55 @@
-import React, { useEffect, useState} from 'react';
+import React, { Component,useEffect, useState} from 'react';
 import SingleContact from './SingleContact';
 import AddContacts from './AddContacts';
 
 //Run inspection on browser to analyze error and fix the code
-function Contacts() {
-    const [contact, setContact] = useState([]);
-
-    useEffect(() => {
-
-
-        document.title = `You clicked ${contact} times`;
-    });
-
-    
-//Basic React will be transformed to React-hooks
-//Code is just a demo
-// export  default class Contacts extends Component{
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             contacts: [],
-//         };
-//     }
+// const Contacts = () => {
+//     const [contact, setContact] = useState([]);
 //
-//     //fetch data will be replaced with Axios
-//     componentDidMount() {
-//         fetch('http://localhost:8080/api/contacts')
-//             .then(response => response.json())
-//             .then(data => this.setState({contacts: data}))
-//     }
+//     useEffect(() => {
 //
 //
-//     render() {
+//         document.title = `You clicked ${contact} times`;
+//     });
 
-        return(
+//
+// Basic React will be transformed to React-hooks
+function setContact() {
+    return undefined;
+}
+
+// Code is just a demo
+export  default class Contacts extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            contacts: [],
+        };
+    }
+
+    //fetch data will be replaced with Axios
+    contact;
+    componentDidMount() {
+        fetch('http://localhost:8080/api/contacts')
+            .then(response => response.json())
+            .then(data => this.setState({contacts: data}))
+    }
+
+
+    render() {
+
+        return (
             <div>
                 <div className="row">
                     <AddContacts/>
 
-                    <button onClick={() => setContact(contact + 1)}>
+                    <button onClick={() => setContact(this.contact + 1)}>
                         Click me
                     </button>
 
                 </div>
                 <div className="row">
-                    { this.state.contacts.map((item) =>(
+                    {this.state.contacts.map((item) => (
                         <SingleContact key={item.id} item={item}/>
                     ))}
 
@@ -52,6 +57,6 @@ function Contacts() {
 
             </div>
         )
-
+    }
 }
-export default new Contacts();
+// export default new Contacts();
